8fcaa99dc71e17f0c0320a7e0e64d96e7734fa31
Ernestas936 <Ernestas936@gmail.com>
2022-04-10 12:06:43 +0300

    Changes

--- new_car/registracija/urls.py
+++ new_car/registracija/urls.py
@@ -1,4 +1,4 @@
-"""new_car URL Configuration
+"""servisas URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('registracija.urls')),
+
+    path('', views.registracija, name='registracija'),
 ]
